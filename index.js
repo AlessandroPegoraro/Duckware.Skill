60e784c4451516601d7d221e5d14e5685bd4962b
soniamenon <soniamenon995@gmail.com>
2019-03-25 13:59:53 +0100

    Va?

--- index.js
+++ index.js
@@ -16,12 +16,34 @@ const LaunchRequestHandler = {
         let speechText = '';
 
         if (!accessToken) {
-            speechText = 'Devi autenticarti con il tuo account Amazon per usare questa skill. Ti ho inviato le istruzioni nella tua App Alexa.';
+            speechText = 'Devi autenticarti con il tuo account Swetlapp per usare questa skill. Ti ho inviato le istruzioni nella tua App Alexa.';
             return handlerInput.responseBuilder
                 .speak(speechText)
                 .withLinkAccountCard()
                 .getResponse();
-        } else {
+        }
+        try {
+            let tokenOptions = buildHttpGetOptions(accessToken);
+
+            let response = await httpGet(tokenOptions);
+            console.log({ response });
+            console.log('Username:' + response.username);
+            const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+            sessionAttributes.username = response.username;
+            /*if (response.nickname.length > 0) {
+                let nickname = response.nickname;
+                const sessionAttributes = attributesManager.getSessionAttributes();
+                sessionAttributes.nickname = nickname;
+                console.log('Nickname: ' + nickname);
+                console.log('Name: ' + response.name);
+            }
+            */
+
+        }
+        catch (error) {
+            console.log(`Error message: ${error.message}`);
+        }
+        /* else {
             const amznProfileUrl = `https://api.amazon.com/user/profile?access_token=${accessToken}`;
 
             try {
@@ -29,19 +51,63 @@ const LaunchRequestHandler = {
                 //console.log(response.data.email);
                 email = response.data.email;
                 const name = response.data.name.split(" ")[0];
-                speechText = `Ciao, ${name}! Benvenuto in SwetlApp, come posso aiutarti?`;
+                speechText = 'Ciao, ${name}! Benvenuto in SwetlApp, come posso aiutarti?';
             } catch (error) {
                 //console.error(error);
                 speechText = 'Si è verificato un errore durante l\'esecuzione. Riprova più tardi.';
             }
-
+            */
+        speechText = 'Ciao ' + handlerInput.attributesManager.getSessionAttributes().username +'! Benvenuto in swetlapp!';
             return handlerInput.responseBuilder
                 .speak(speechText)
                 .reprompt(speechText)
                 .getResponse();
-        }
+        //}
     }
 };
+
+//Helper Function for calling the Cognito /oauth2/userInfo to get user info using the accesstoken
+let https = require('https');
+// https is a default part of Node.JS.  Read the developer doc:  https://nodejs.org/api/https.html
+function buildHttpGetOptions(accessToken) {
+    return {
+        //Replace the host with your cognito user pool domain
+        host: 'swetlapp-prova.auth.eu-west-1.amazoncognito.com',
+        port: 443,
+        path: '/oauth2/userInfo',
+        method: 'GET',
+        headers: {
+            'authorization': 'Bearer ' + accessToken
+        }
+    };
+}
+
+function httpGet(options) {
+    return new Promise(((resolve, reject) => {
+        let request = https.request(options, (response) => {
+            response.setEncoding('utf8');
+            let returnData = '';
+
+            if (response.statusCode < 200 || response.statusCode >= 300) {
+                return reject(new Error(`${response.statusCode}: ${response.req.getHeader('host')} ${response.req.path}`));
+            }
+
+            response.on('data', (chunk) => {
+                returnData += chunk;
+            });
+
+            response.on('end', () => {
+                console.log({ returnData });
+                resolve(JSON.parse(returnData));
+            });
+
+            response.on('error', (error) => {
+                reject(error);
+            });
+        });
+        request.end();
+    }));
+}
 
 const RunWorkflowHandler = {
     canHandle(handlerInput) {
@@ -193,7 +259,6 @@ exports.handler = async function (event, context) {
         skill = Alexa.SkillBuilders.standard()
             .addRequestHandlers(
                 LaunchRequestHandler,
-                ReadRSSHandler,
                 RunWorkflowHandler,
                 WorkflowRepeatHandler,
                 HelpIntentHandler,
